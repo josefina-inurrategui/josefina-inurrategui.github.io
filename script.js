7cfef21fcfdf64c99ee90e6f5344c461f8f0acf2
Josefina Iñurrategui <40969611+josefina-inurrategui@users.noreply.github.com>
2021-07-20 17:45:31 -0300

    Add files via upload
    Agregando archivos

---
+++ script.js
@@ -0,0 +1,15 @@
+const p = document.getElementById("nombre"); 
+const boton = document.getElementById("boton");
+
+boton.addEventListener("click", () => {
+  p.innerHTML = "hiciste click"
+}) 
+
+
+
+/* function suma (a,b){
+  const c = a+b 
+  console.log(c)
+}
+
+ */
